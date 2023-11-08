584a1eb7f0bcb49187dfd84d738ae15414d0a82b
komu <sanjeevniraj2001@gmail.com>
2023-11-08 18:47:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import SearchForm from './SearchForm';
+import ThemeToggle from './ThemeToggle';
+import Gallery from './Gallery';
+const App = () => {
+  return (
+    <>
+      <ThemeToggle />
+      <SearchForm />
+
+      <Gallery />
+    </>
+  );
+};
+export default App;
